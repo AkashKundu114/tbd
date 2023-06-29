e470f1e999cd1be3be0674a8f48ff7d9e61179f5
AkashKundu114 <68686532+AkashKundu114@users.noreply.github.com>
2023-06-29 22:53:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import users from "./users.json" assert { type: "json" };
+
+var form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    let n = 0;
+    for (let i = 0; i <= 11; i++) {
+        if (users[i]["name"] == username && users[i]["password"] == password) {
+            window.location = "http://www.example.com";
+        }
+        
+} if (username === "") {
+    document.getElementById("show").innerHTML =
+        "<p id='error'>Please enter an Username!</p>";
+
+} if (password === "") {
+    document.getElementById("show").innerHTML =
+        "<p id='error'>Please enter an Password!</p>";
+
+} if (username === "" && password === "") {
+    document.getElementById("show").innerHTML =
+        "<p id='error'>Please enter Username and Password!</p>";
+}  {
+    document.getElementById("show").innerHTML =
+        "<p id='error'>Incorrect username or password!!</p>";
+
+}
+
+});
